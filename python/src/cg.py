1734640c8234c995ae6a60c8ab3e682e32f86da8
Alex Buccheri <alexanderbuccheri@googlemail.com>
2025-02-01 23:48:33 +0000

    Implement the BFGS algorithm. Requires testing.

--- python/src/cg.py
+++ python/src/cg.py
@@ -93,7 +93,7 @@ def preconditioned_conjugate_gradient(A: np.ndarray,
 # Define a function that evaluates a vector to a float.
 FuncType = Callable[[np.ndarray], float]
 
-# Define a derivative that accepts a vector and returns a vector
+# Define a derivative that accepts a vector and returns a vector of the same length.
 DerFuncType = Callable[[np.ndarray], np.ndarray]
 
 
@@ -102,10 +102,10 @@ def fletcher_reeves_coefficient(g: np.ndarray, g_next: np.ndarray) -> float:
 
 
 def nonlinear_conjugate_gradient(f: FuncType,
-                                 df: FuncType,
+                                 df: DerFuncType,
                                  x0: np.ndarray,
-                                 n_iter=100,
-                                 tol=1.e-8):
+                                 n_iter=500,
+                                 tol=1.e-6):
     """Non-linear conjugate gradient.
 
     Note: The current implementation is not that robust.
@@ -152,6 +152,94 @@ def nonlinear_conjugate_gradient(f: FuncType,
     return x, n_iter
 
 
-# Implement me
-def bfgs_optimiser():
-    return
+def update_hessian(s: np.ndarray, y: np.ndarray, Hess: np.ndarray) -> np.ndarray:
+    """ Update the Hessian in BFGS.
+
+    Maintain the symmetry and positive definiteness of Hₖ
+    TODO Add expression for this approximation update.
+
+    :param s: Change in the iterates, x_{k+1} = x_k
+    :param y: Change in the gradients, grad_{k+1} - grad_k
+    :param Hess: Current approximate inverse Hessian
+    :return: Updated approximate inverse Hessian.
+    """
+    n = np.size(s)
+    assert np.size(y) == n, "s and y must have same size"
+    assert Hess.shape == (n, n)
+    # If this is not true, the line search is likely the issue
+    assert np.dot(y, s) > 0, "Expect y dot s > 0"
+
+    p = 1. / (np.dot(y, s))
+    id = np.eye(n, n)
+
+    return (id - p * (s @ y.T)) @ Hess @ (id - p * (y @ s.T)) + (p * s @ s.T)
+
+
+def bfgs_optimiser(f: FuncType,
+                   df: DerFuncType,
+                   x0: np.ndarray,
+                   max_iter=500,
+                   tol=1.e-6):
+    """BFGS.
+
+    Scaling of the Hessian in the first iteration:
+    This scaling is motivated by the secant condition used in quasi-Newton methods, ensuring that
+    Hess_0 has a reasonable initial curvature estimate. The fraction (add expression) approximates the
+    inverse of the Hessian’s first step curvature.
+
+    This is not needed if the function, f, is well-conditioned.
+
+    :param f:  Function to optimise
+    :param df: Derivative of f
+    :param x0: Initial guess for x
+    :param max_iter: Maximum number of iterations to use
+    :param tol: Convergence threshold in the gradient
+    :return: x: Vector that minimises f(x)
+    """
+    n = np.size(x0)
+    # Initialise variable
+    x = np.copy(x0)
+    # Initialise approximate inverse Hessian
+    # TODO Scale the diagonal with what?
+    Hess = np.eye(n, n)
+    # Initialise gradient at x0
+    grad = df(x)
+    # Initialise search direction
+    d = - Hess @ grad
+    # Initialise step size
+    alpha = 1
+
+    for k in range(0, max_iter):
+        # Line search
+        alpha = line_search_backtrack(f, df, x, d, alpha)
+
+        # save x_k in s
+        s = np.copy(x)
+        # Update current point, x_{k+1}
+        x += alpha * d
+
+        # Save gradient, grad_k, in y
+        y = np.copy(grad)
+        # Compute new gradient
+        grad: np.ndarray = df(x)
+
+        # Check convergence
+        if np.linalg.norm(grad) <= tol:
+            return x, k
+
+        # Compute change vectors
+        s = x - s
+        y = grad - y
+
+        # Scale with an estimate of the curvature of the function at the starting point
+        if k == 0:
+            scaling_factor = np.dot(s, y) / np.dot(y, y)
+            Hess *= scaling_factor
+
+        # Update the inv Hessian approximation
+        Hess = update_hessian(s, y, Hess)
+
+        # Update search direction
+        d = -Hess @ grad
+
+    return x, max_iter
