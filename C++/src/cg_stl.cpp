a2d05e85b7c2db654a077f7eed017e1a01e366c0
Alex Buccheri <alexanderbuccheri@googlemail.com>
2025-02-07 22:59:42 +0000

    Implement linear CG using std::vector. This needs testing/validating.

--- C++/src/cg_stl.cpp
+++ C++/src/cg_stl.cpp
@@ -1,10 +1,11 @@
 /*
  * Implementation of linear algebra routines using the STL and corresponding containers/
  */
-#include <vector>
+#include <cassert>
 #include <cmath>
 #include <numeric>
 #include <stdexcept>
+#include <vector>
 
 #include "cg_stl.h"
 
@@ -16,38 +17,96 @@ namespace algorithms {
             throw std::invalid_argument("Dimension mismatch in dot product.");
         }
         // See https://en.cppreference.com/w/cpp/algorithm/inner_product
-        return std::inner_product(u.begin(), u.end(), v.begin(), 0);
+        return std::inner_product(u.begin(), u.end(), v.begin(), 0.0);
     }
 
     double norm(const std::vector<double> & v) {
         return std::sqrt(dot(v, v));
     }
 
-    // TODO(Alex). Look at the algorithms header, and at BLAS wrappers for C++
+    std::vector<double> matrix_vector_product(const matrix & a,
+                                              const std::vector<double> & x){
+        // TODO Check these work as expected
+        const uint n_row = a.size();
+        const uint n_col = a[0].size();
+        assert(x.size() == n_row);
+
+        std::vector<double> b(n_row, 0.0);
+        for (int i = 0; i < n_row; ++i) {
+            for (int j = 0; j < n_col; ++j) {
+                 b[i] += a[i][j] * x[j];
+            }
+        }
+
+        return b;
+    }
+
+    std::vector<double> vector_subtract(const std::vector<double> & u,
+                                        const std::vector<double> & v){
+        if (u.size() != v.size()) {
+            throw std::invalid_argument("Dimension mismatch in dot product.");
+        }
+
+        std::vector<double> u_minus_v(u.size());
+
+        for (int i = 0; i < u.size(); ++i) {
+            u_minus_v.push_back(u[i] - v[i]);
+        }
+
+        return u_minus_v;
+    }
+
+    std::vector<double> vector_add(const std::vector<double> & u,
+                                   const std::vector<double> & v){
+        if (u.size() != v.size()) {
+            throw std::invalid_argument("Dimension mismatch in dot product.");
+        }
+
+        std::vector<double> u_plus_v(u.size());
+
+        for (int i = 0; i < u.size(); ++i) {
+            u_plus_v.push_back(u[i] + v[i]);
+        }
+
+        return u_plus_v;
+    }
+
+    std::vector<double> vector_scale(const std::vector<double> & u,
+                                     const double alpha){
+        std::vector<double> u_scaled(u.size());
+        for(auto element : u){
+            u_scaled.push_back(element * alpha);
+        }
+
+        return u_scaled;
+    }
+
+    // TODO(Alex). Look at BLAS wrappers for C++
     std::vector<double> linear_conjugate_gradient(const matrix & a,
                                                   const std::vector<double> & x0,
                                                   const std::vector<double> & b,
                                                   const double tol,
                                                   const int max_iter){
         std::vector<double> x = x0;
-        std::vector<double> r; // MV product, followed by subtraction
+        auto r = vector_subtract(b, matrix_vector_product(a, x));
         if (norm(r) < tol) {
             return x;
         }
-        std::vector<double> p = r;
+        auto p = r;
 
         for (int k = 0; k < max_iter; ++k) {
-            const std::vector<double> ap; // TODO Matrix-vector product
+            const auto ap = matrix_vector_product(a, p);
             const double alpha = dot(r, r) / dot(p, ap);
-            // TODO x = x + alpha * p
-            std::vector<double> r_next; // MV product, followed by subtraction
+            const auto alpha_times_p = vector_scale(p, alpha);
+            x = vector_add(x, alpha_times_p);
+            const auto r_next = vector_subtract(r, ap);
 
             if (norm(r_next) < tol) {
                 return x;
             }
 
             const double beta = dot(r_next, r_next) / dot(r, r);
-            // TODO Update p
+            p = vector_add(r_next, vector_scale(p, beta));
             r = r_next;
         }
 
